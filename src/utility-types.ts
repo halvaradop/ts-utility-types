72a4f0dc0c6e00a4e79ee31246427f855b78d1c5
Hernan Alvarado <105169289+halvaradop@users.noreply.github.com>
2024-07-13 16:59:55 -0500

    chore: add tuple-to-union type

--- src/utility-types.ts
+++ src/utility-types.ts
@@ -18,3 +18,15 @@ export type DeepReadonly<T extends object> = {
             ? DeepReadonly<T[Property]>
             : T[Property];
 };
+
+/**
+ * Creates a union type from the literal values of a constant string or number array.
+ * This is useful for representing a set of allowed values based on the array elements
+ * 
+ * @example
+ * type StringUnion = ["1", "2", "3"]
+ * type Union = TypleToUnion<StringUnion> // "1" | "2" | "3"
+ */
+export type TupleToUnion<T extends readonly any[]> = T extends [infer Item, ...infer Spreed]
+    ? Item | TupleToUnion<Spreed>
+    : never;
