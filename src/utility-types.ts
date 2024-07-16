5607cc82027339066410d00baae6010cee4eee93
Hernan Alvarado <105169289+halvaradop@users.noreply.github.com>
2024-07-16 12:55:17 -0500

    feat: add size, last and pop utility types

--- src/utility-types.ts
+++ src/utility-types.ts
@@ -30,3 +30,30 @@ export type DeepReadonly<T extends object> = {
 export type TupleToUnion<T extends readonly any[]> = T extends [infer Item, ...infer Spreed]
     ? Item | TupleToUnion<Spreed>
     : never;
+
+
+/**
+ * Gets the length (size) of an array.
+ *
+ * @example
+ * const numbers: number[] = [1, 2, 3, 4, 5];
+ * type SizeOfNumbers = Size<typeof numbers>; // SizeOfNumbers = 5
+ */
+export type Size<T extends any[]> = T extends any[] ? T["length"] : 0;
+
+
+/**
+ * Gets the type of the last element in an array, or `never` if the array is empty.
+ * @example
+ * type LastItem = Last<1, 2, 3, 4> // 4
+ */
+export type Last<T extends any[]> = T extends [...any, infer Last] ? Last : never;
+
+/**
+ * Removes the last element from an array and returns a new array type with all elements 
+ * except the last. If the array is empty, returns an empty array type
+ * @example
+ * type PopStr = Pop<["a", "b", "c"]> // ["a", "b"]
+ * type PopNums = Pop<[1, 2, 3]> // [1, 2]
+ */
+export type Pop<T extends any[]> = T extends [...infer Items, any] ? Items : [];
