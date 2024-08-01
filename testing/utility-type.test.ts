b939ff3e71df27c6a46586e424ecfcd7385dcc53
Hernan Alvarado <105169289+halvaradop@users.noreply.github.com>
2024-08-01 16:13:32 -0500

    feat: add `PublicType` utility type

--- testing/utility-type.test.ts
+++ testing/utility-type.test.ts
@@ -1,5 +1,5 @@
 import { describe, test, expectTypeOf } from "vitest"
-import type { Capitalize, Uppercase, Lowercase, TrimLeft, TrimRight, Trim, Merge, Properties, ExtractToObject } from "../src/utility-types"
+import type { Capitalize, Uppercase, Lowercase, TrimLeft, TrimRight, Trim, Merge, Properties, ExtractToObject, PublicType } from "../src/utility-types"
 
 
 describe("String mappers", () => {
@@ -69,5 +69,14 @@ describe("Extract property from object", () => {
     test("Extract the key", () => {
         expectTypeOf<ExtractToObject<{ name: string, address: { street: string } }, "address">>().toEqualTypeOf<{ name: string, street: string }>()
         expectTypeOf<ExtractToObject<{ name: string, address: { street: string, avenue: string } }, "address">>().toEqualTypeOf<{ name: string, street: string, avenue: string }>()
+    })
+})
+
+
+describe("PublicType", () => {
+    test("Remove properties beginning with (_)", () => {
+        expectTypeOf<PublicType<{ foo: string }>>().toEqualTypeOf<{ foo: string }>();
+        expectTypeOf<PublicType<{ foo: string, _bar: string }>>().toEqualTypeOf<{ foo: string }>();
+        expectTypeOf<PublicType<{ _foo: string, _bar: string }>>().toEqualTypeOf<{}>();
     })
 })
