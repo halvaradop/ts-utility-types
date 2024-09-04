03f67ae479c3ca0eda0802c22c0df2bd89de1821
Hernan Alvarado <hernanvid123@gmail.com>
2024-09-04 09:47:45 -0500

    chore(docs): improve utility types examples (#102)

--- src/test.ts
+++ src/test.ts
@@ -1,9 +1,23 @@
 /**
  * Checks if two values are strictly equal, regardless of their distribution within conditional types
+ *
+ * @example
+ * // Expected: true
+ * type CheckTrue = Equals<true, true>;
+ *
+ * // Expected: false
+ * type CheckTrue = Equals<() => {}, true>;
  */
 export type Equals<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y ? 1 : 2 ? true : false;
 
 /**
  * Ensures that the parameter is true.
+ *
+ * @example
+ * // Expected: true
+ * type CheckTrue = Expect<true>;
+ *
+ * // Expected: false
+ * type CheckFalse = Expect<false>;
  */
 export type Expect<T extends true> = T;
