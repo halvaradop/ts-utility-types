d335b2c26f6f0cb69b55840464a639fbe2d07038
Hernan Alvarado <hernanvid123@gmail.com>
2024-10-02 19:49:42 -0500

    chore: clean up the codebase (#112)

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,13 +1,27 @@
 import { defineConfig } from "tsup";
 
+/**
+ * Configuration for the tsup bundler. This configuration specifies two main entry points
+ * for building the package. The first entry point builds the utility types, generating only
+ * the .d.ts files. The second entry point handles files that need to be built in both .js and
+ * .cjs formats.
+ */
 export const tsup = defineConfig([
 	{
-		entry: ["src/*.ts"],
+		entry: ["src", "!src/validate-types.ts"],
 		format: ["esm"],
 		outDir: "dist",
 		dts: {
 			only: true,
 		},
+		minify: true,
+		clean: true,
+	},
+	{
+		entry: ["src/validate-types.ts"],
+		format: ["esm", "cjs"],
+		outDir: "dist/utils",
+		dts: true,
 		minify: true,
 		clean: true,
 	},
