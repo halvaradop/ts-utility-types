caeddbc7d79093eb264ea5a3c6668a0802ef09d4
Hernan Alvarado <hernanvid123@gmail.com>
2025-06-05 11:00:24 -0500

    docs(changelog): update `CHANGELOG.md` file

---
+++ src/types.ts
@@ -0,0 +1,9 @@
+/**
+ * @deprecated
+ * This file is deprecated and will be removed in the next major release.
+ * Please use the `@halvaradop/ts-utility-types` default export instead
+ * or see the others exports for specific utility types.
+ */
+throw new Error(
+    "[DEPRECATED] `/types entry` point is deprecated and will be removed in the next major release. Please use the default export from `@halvaradop/ts-utility-types` instead. Or use the specific utility types you need from the package directly."
+)
