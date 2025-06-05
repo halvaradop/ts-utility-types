46fe98f21ca35464c83d263bb8ee6681f03345c8
Hernan Alvarado <hernanvid123@gmail.com>
2025-06-05 11:24:04 -0500

    docs(changelog): update `CHANGELOG.md` file (#180)

--- src/utility-types.ts
+++ src/utility-types.ts
@@ -5,5 +5,5 @@
  * or see the others exports for specific utility types.
  */
 throw new Error(
-    "[DEPRECATED] utility-types entry point is deprecated and will be removed in the next major release. Please use the default export from `@halvaradop/ts-utility-types` instead. Or use the specific utility types you need from the package directly."
+    "[DEPRECATED] `/utility-types` entry point is deprecated and will be removed in the next major release. Please use the default export from `@halvaradop/ts-utility-types` instead. Or use the specific utility types you need from the package directly."
 )
