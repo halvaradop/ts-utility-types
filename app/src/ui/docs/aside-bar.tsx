baadb51f85dec7797e4d062c603a03fc61dea230
Hernan Alvarado <hernanvid123@gmail.com>
2025-06-21 10:13:17 -0500

    docs(mdx): update `.mdx` files

--- app/src/ui/docs/aside-bar.tsx
+++ app/src/ui/docs/aside-bar.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { Select, SelectList, SelectOption, SelectTrigger } from "@halvaradop/ui-select"
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 const entryPoints = [
     "deep",
@@ -17,18 +18,25 @@ const entryPoints = [
 ]
 
 export const AsideBar = () => {
+    const pathname = usePathname()
+    const defaultEntryPoint = pathname.replace("/docs/", "")
+
     return (
         <aside className="relative mb-4">
-            <Select className="mt-4" name="entry-points">
+            <Select className="mt-4 mx-0" name="entry-points" defaultValue={defaultEntryPoint}>
                 <SelectTrigger className="w-full hover:border-secondary hover:bg-secondary">Entry Points</SelectTrigger>
                 <SelectList className="button:hover:bg-secondary">
                     {entryPoints.map((entryPoint) => (
                         <SelectOption
-                            className="text-primary aria-selected:text-primary aria-selected:bg-secondary"
+                            className="p-0 text-primary aria-selected:text-primary aria-selected:bg-secondary"
                             key={entryPoint}
                             value={entryPoint}
+                            tabIndex={-1}
                         >
-                            <Link className="w-full text-left" href={`/docs/${entryPoint}`}>
+                            <Link
+                                className="w-full h-full px-[calc(var(--size-base)*0.4)] flex items-center text-left"
+                                href={`/docs/${entryPoint}`}
+                            >
                                 {entryPoint}
                             </Link>
                         </SelectOption>
