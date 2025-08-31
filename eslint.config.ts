c9cae6da57ec98f902ee80efe81a7f32569fbbf4
Hernan Alvarado <hernanvid123@gmail.com>
2025-08-31 16:20:23 -0500

    chore: set up `@halvaradop/tsconfig` package

--- eslint.config.ts
+++ eslint.config.ts
@@ -4,4 +4,4 @@ import { reactConfig } from "@halvaradop/eslint-config/react"
 import { prettierConfig } from "@halvaradop/eslint-config/prettier"
 import { tsConfig } from "@halvaradop/eslint-config/typescript"
 
-export default [...baseConfig,...nextConfig,...reactConfig,...prettierConfig,...tsConfig]
+export default [...baseConfig, ...nextConfig, ...reactConfig, ...prettierConfig, ...tsConfig]
